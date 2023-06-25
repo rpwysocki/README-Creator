da68f675bafdff5efbc37b15232765055bba11b3
rpwysocki <134246531+rpwysocki@users.noreply.github.com>
2023-06-25 00:38:16 -0400

    initial setup

---
+++ index.js
@@ -0,0 +1,24 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+
+const inquirer = require('inquirer');
+
+const err = (err) => {
+    if (err) throw err;
+};
+
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    
+    console.log('Initializing app');
+}
+
+// Function call to initialize app
+init();
