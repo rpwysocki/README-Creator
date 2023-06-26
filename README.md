5e9ee12edcb7d88a14a9c5644680f0937c48bd98
rpwysocki <134246531+rpwysocki@users.noreply.github.com>
2023-06-25 23:54:02 -0400

    Updates made

---
+++ README.md
@@ -0,0 +1,7 @@
+
+    Title: README.md
+    Description: How to
+    Installation: Run on command line
+    Usage: Create a README.md file
+    License: MIT License
+    
--- index.js
+++ index.js
@@ -1,24 +1,65 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const fs = require('fs');
 
 const inquirer = require('inquirer');
+const { default: Choice } = require('inquirer/lib/objects/choice');
+const { default: Choices } = require('inquirer/lib/objects/choices');
 
 const err = (err) => {
     if (err) throw err;
 };
 
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Questions for user input
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+inquirer.prompt([
+    {
+        name: 'title',
+        message: 'Please enter your project title.'
+    },
+    {
+        name: 'description',
+        message: 'Enter the project description.'
+    },
+    {
+        name: 'installation',
+        message: 'Please enter installation instructions.'
+    },
+    {
+        name: 'usage',
+        message: 'Please enter the purpose of the project.'
+    },
+    {
+        name: 'license',
+        message: 'Please choose a license.',
+        type: 'list',
+        choices:['MIT License', 'Apache License 2.0', 'GNU General Public License v3.0', 'Boost Software License 1.0']
+
+    }
+
+]).then(({title, description, installation, usage, license}) => {
+    const questions = `
+    Title: ${title}
+    Description: ${description}
+    Installation: ${installation}
+    Usage: ${usage}
+    License: ${license}
+    `
+    fs.writeFile('./README.md', questions, err);
+});
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    
+
     console.log('Initializing app');
 }
 
 // Function call to initialize app
-init();
+// init();
+
+
+// Description, Installation, Usage, License, Contributing, Tests, and Questions
